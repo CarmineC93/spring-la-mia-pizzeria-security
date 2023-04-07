76e36dbd69f24a1e1a39e53b88f52fafc8a683d0
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-04-07 14:35:45 +0200

    add: dependencies security, user model

---
+++ src/main/java/org/excercise/pizzeria/springlamiapizzeriacrud/model/User.java
@@ -0,0 +1,66 @@
+package org.excercise.pizzeria.springlamiapizzeriacrud.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstName;
+    private String lastName;
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
