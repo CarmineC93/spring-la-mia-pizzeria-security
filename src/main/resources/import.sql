82aabbc0a72c6922dee5847c204597edd15c09ae
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-04-07 15:02:46 +0200

    add: DatabaseUserDetails that extends userDetails

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,12 @@ INSERT INTO pizzas (name, description, price) VALUES('diavola', 'roba piccantina
 INSERT INTO ingredients (name, description) VALUES('pomodorino', 'biologico');
 INSERT INTO ingredients (name, description) VALUES('mozzarella', 'km0');
 INSERT INTO ingredients (name, description) VALUES('farina', 'farina di semola');
+
+INSERT INTO users (email, first_name, last_name, password) VALUES('pippo@email.it', 'Pippo', 'Pippetto', '{noop}pippo123');
+INSERT INTO users (email, first_name, last_name, password) VALUES('pluto@email.it', 'Pluto', 'Plutino', '{noop}pluto123');
+
+INSERT INTO roles (id, name) VALUES(1, 'ADMIN');
+INSERT INTO roles (id, name) VALUES(1, 'USER');
+
+INSERT INTO users_roles(user_id, roles_id) VALUES (1,1);
+INSERT INTO users_roles(user_id, roles_id) VALUES (2,2);
